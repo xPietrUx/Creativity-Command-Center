e58c610d99022c930f392defc0e18ec746c85826
Piotr <piotrdusinski01@gmail.com>
2024-02-02 19:40:29 +0100

    first commit

---
+++ src/components/MouseHighlight/MouseHighlight.tsx
@@ -0,0 +1,8 @@
+import { useEffect, useState } from 'react';
+import './style.scss';
+
+const MouseHighlight = () => {
+  //TODO useReducer() with initialState { cursorPosition: { x: 0, y: 0}, isMouseOver:false}
+  // return <div className={}></div>;
+};
+export default MouseHighlight;
