6213e65639f7c9b49b045a20aa2281ec30b79066
Piotr <piotrdusinski01@gmail.com>
2024-02-19 11:36:50 +0100

    Adding Calendar and Informant to main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './style.scss';
-import ShouldLoadMouseHighlight from './components/MouseHighlight/shouldLoadMouseHighlight';
+import ShouldLoadMouseHighlight from './components/MouseHighlight/ShouldLoadMouseHighlight';
+import Calendar from './components/Calendar/Calendar';
+import Informant from './components/Informant/Informant';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ShouldLoadMouseHighlight />
+    <Informant />
+    <Calendar />
   </React.StrictMode>
 );
